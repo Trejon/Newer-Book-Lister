bdd4f168431db9cc71009153b3d0b00c5a5755ba
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-05-18 00:21:20 -0600

    changed db to postgres

--- book_lister_backend/db/schema.rb
+++ book_lister_backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_19_222352) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
